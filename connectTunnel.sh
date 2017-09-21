082c7c979da9df546c595d2231011d0613abe988
Andrew Yates <yatesab@gmail.com>
2017-09-21 15:51:36 -0600

    Log handling
    Changed the way that the script logged its actions. This file now uses the universal logs script to put out its log files.

--- connectTunnel.sh
+++ connectTunnel.sh
@@ -5,7 +5,7 @@ VPNFILE="$(ls /etc/randomVPN/locations/ | sort -R | tail -n1)"
 # Need a log system here
 if openvpn /etc/randomVPN/locations/$VPNFILE
 then
-	sudo echo "$(date) Success Tunnel Open" >> ./logs/"$(date)"-Success-TO.txt
+	./log.sh "TunnelOpen_Success"
 else
-	sudo echo "$(date) Error Failed Connect" >> ./logs/"$(date)"-Error-FC.txt
+	./log.sh "FailedConnection"
 fi
