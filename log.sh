4a516f45acbb8e347f06bc0a91a5a7e3965f25e8
Andrew Yates <yatesab@gmail.com>
2017-09-21 15:54:32 -0600

    Comments & Structure
    Changed the Structure of the error messages to make it easier to read. Also added comments for each type of error to better explain why they may happen.

--- log.sh
+++ log.sh
@@ -2,31 +2,35 @@
 
 PATH=$PATH:/etc/randomVPN
 
+#checks to see if anything was passed in.
+#If nothing was passed we exit with nothing done.
 if [ $# -ne 0 ]
 then
-	#Tunnel is already open error
-	if [ $1 = "TunnelOpen_Error" ]
-  	then
-    		echo "Tunnel is already open"
-    		echo "$(date) TunnelOpen Error \n" >> ./logs/"$(date)"-Error-TO.txt
-	elif [ $1 = "TunnelOpen_Success" ]
-	then
-		echo "Tunnel Successfully Opened"
-		echo "$(date) Tunnel Opened Success" >> ./logs/$DATE-Success-TO.txt
-	elif [ $1 = "NoArg" ]
+	#Runs if no arguments were passed into the program
+	if [ $1 = "NoArg" ]
 	then
 		echo "No Arguments Passed"
 		echo "Examples:"
 		echo "-l - Show logs"
 		echo "-s - Start Tunnel"
+	#If tunnel is successfull in opening then this log will be created.
+	elif [ $1 = "TunnelOpen_Success" ]
+	then
+		echo "Tunnel Successfully Opened"
+		echo "$(date) Tunnel Opened Success" >> ./logs/$DATE-Success-TO.txt
+	#Tunnel is already open error
+	elif [ $1 = "TunnelOpen_Error" ]
+  	then
+    		echo "Tunnel is already open"
+    		echo "$(date) TunnelOpen Error \n" >> ./logs/$DATE-Error-TO.txt
+	#If the connection fails while trying to establish a tunnel
+	elif [ $1 = "FailedConnection" ]
+	then
+		echo "Tunnel failed to connect"
+		echo "$(date) Error Failed Connect" >> ./logs/$DATE-Error-FC.txt
   	#No configuration files found in folder
   	elif [ $1 = "NoConfig" ]
 	then
 		echo "No config files were found"
 		echo "$(date) NoConfig Error \n" >> ./logs/$DATE-Error-NOCONF.txt
-	elif [ $1 = "FailedConnection" ]
-	then
-		echo "Connection to the VPN Server failed"
-		echo "$(date) FailedConnection Error \n" >> ./logs/$DATE-Error-FC.txt
-  	fi
 fi
